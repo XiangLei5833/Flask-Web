d2e19bdf930c21bfe4c0d391e2a97b40b8f15c89
XiangLei <leixiang5833@163.com>
2017-12-27 15:43:34 +0800

    First-Day

--- hello.py
+++ hello.py
@@ -6,9 +6,5 @@ app = Flask(__name__)
 def index():
     return '<h1>Hello World</h>'
 
-@app.route('/user/<name>')
-def user(name):
-    return '<h1>Hello, %s!</h1>' % name
-
 if __name__ == '__main__':
     app.run(debug=True)
